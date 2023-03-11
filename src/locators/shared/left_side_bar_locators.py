e81cd8daf9b6de938fb79a373115be7f9139a36e
Elik Zakai <elik.zakai@gmail.com>
2023-03-11 10:15:29 +0200

    changed the locators to XPATH

--- src/locators/shared/left_side_bar_locators.py
+++ src/locators/shared/left_side_bar_locators.py
@@ -4,11 +4,11 @@ class Left_side_bar_locators:
     admin_xpath = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Admin']"
     pim_xpath = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='PIM']"
     leave_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Leave']"
-    time_text = 'Time'
-    recruitment_text = 'Recruitment'
-    my_info_text = 'My info'
-    performance_text = 'Performance'
-    dashboard_text = 'Dashboard'
-    directory_text = 'Directory'
-    maintenance_text = 'Maintenance'
-    buzz_text = 'Buzz'
+    time_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Time']"
+    recruitment_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Recruitment']"
+    my_info_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='My info']"
+    performance_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Performance']"
+    dashboard_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Dashboard']"
+    directory_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Directory']"
+    maintenance_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Maintenanc']"
+    buzz_text = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and text()='Buzz']"
